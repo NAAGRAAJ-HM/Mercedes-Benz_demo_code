049ae0018cbff8375f75751e02bcbf603219702d
NAAGRAAJ HM <nagaraja.hm.0001@gmail.com>
2022-05-06 06:51:35 +0200

    Add files via upload

---
+++ sample_03a/test_code.c
@@ -0,0 +1,33 @@
+#include "test_code.h"
+
+#define L    1000000
+#define m          1
+#define M 1000000000
+#define p          0
+
+#include <stdlib.h>
+int solution(
+      int A[]
+   ,  int N
+){
+   int* p_c = (int*)calloc(M+1, sizeof(int));
+   int i = 0;
+   for(
+      i = 0;
+      i < N;
+      i ++
+   ){
+      p_c[A[i]]++;
+   }
+   for(
+      i = m;
+      i < M+m;
+      i ++
+   ){
+      if(1 == p_c[i]){
+         break;
+      }
+   }
+   free(p_c);
+   return i;
+}
