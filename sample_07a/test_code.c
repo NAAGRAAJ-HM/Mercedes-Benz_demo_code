4425d7751ff4bff30781306bfdfb452d4f809cb9
NAAGRAAJ HM <nagaraja.hm.0001@gmail.com>
2022-05-07 22:29:00 +0200

    Add files via upload

---
+++ sample_07a/test_code.c
@@ -0,0 +1,24 @@
+#include "test_code.h"
+#if(1 == _DEBUG_)
+#include <stdio.h>
+#endif
+
+#define l       1
+#define L 1000000
+#define m       1
+#define M 1000000
+
+#include <stdlib.h>
+int solution(int X,int A[],int N){
+   int* D=(int*)calloc(X,sizeof(int));
+   int t;
+   for(t=0;t<N;t++){
+      D[A[t]-1]=t+1;
+      int d;
+      for(d=0;d<X;d++)if(0==D[d])break;
+      if(X==d)break;
+   }
+   free(D);
+   if(N==t) return -1;
+   else     return  t;
+}
