ef43fa70e8668ba57fef353bab1307cd31058ce7
NAAGRAAJ HM <nagaraja.hm.0001@gmail.com>
2022-05-08 23:56:41 +0200

    Add files via upload

---
+++ sample_03c/test_code.c
@@ -0,0 +1,30 @@
+#include "test_code.h"
+#if(1 == _DEBUG_)
+#include <stdio.h>
+#endif
+
+#define m          1
+#define M 1000000000
+#define p 0
+#include <stdlib.h>
+
+#define L 1000000
+int solution(int A[],int N){
+   int i,j,r=-1;
+   if(N<=L){
+      for(i=0;i<N;i++){
+         for(j=i+1;j<N;j++){
+            if(A[i]==A[j]){
+               A[j]=A[i+1];
+               i++;
+               break;
+            }
+         }
+         if(N==j){
+            r=A[i];
+            break;
+         }
+      }
+   }
+   return r;
+}
