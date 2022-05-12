a136cccd36740367ab88fdf093d0df4330283b1c
NAAGRAAJ HM <nagaraja.hm.0001@gmail.com>
2022-05-12 23:25:14 +0200

    Update test_code.c

--- sample_01b/test_code.c
+++ sample_01b/test_code.c
@@ -8,7 +8,7 @@ int solution(int A[],int N){
    int i,M=Cm;
    for(i=0;i<N;i++)if(M<A[i])M=A[i];
    int* p_c=(int*)calloc(M,sizeof(int));
-   for(i=0;i<N;i++)p_c[A[i]-1]++;
+   for(i=0;i<N;i++)if(0<A[i])p_c[A[i]-1]++;
    for(i=0;i<M;i++)if(0==p_c[i])break;
    free(p_c);
    return i+1;
