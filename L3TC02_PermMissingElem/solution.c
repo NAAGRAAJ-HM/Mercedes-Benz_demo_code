664112c8600b75d27d9b7e52d4a2be2f800226f6
NAAGRAAJ HM <nagaraja.hm.0001@gmail.com>
2022-05-14 15:43:48 +0200

    Update solution.c

--- L3TC02_PermMissingElem/solution.c
+++ L3TC02_PermMissingElem/solution.c
@@ -7,7 +7,7 @@
 
 #include <stdlib.h>
 int solution(int A[],int N){
-   int* p_c = (int*)calloc(N+1, sizeof(int));
+   int* p_c=(int*)calloc(N+1,sizeof(int));
    int i;
    for(i=0;i<N;i++)p_c[A[i]]++;
    for(i=1;i<N+1;i++)if(0==p_c[i])break;
