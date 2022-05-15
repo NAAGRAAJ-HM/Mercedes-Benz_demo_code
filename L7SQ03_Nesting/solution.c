77d8911cfabaa05f8cf8b48e84d24d3e161f3eb3
NAAGRAAJ HM <nagaraja.hm.0001@gmail.com>
2022-05-15 21:36:22 +0200

    Add files via upload

---
+++ L7SQ03_Nesting/solution.c
@@ -0,0 +1,76 @@
+#include "solution.h"
+#if(1 == _DEBUG_)
+#include <stdio.h>
+#endif
+
+#include <stdlib.h>
+#define UndefinedElement '\0'
+typedef char typeStackElement;
+typedef struct{
+   typeStackElement* ptrBuffer;
+   int               StackPointer;
+   int               StackDepth;
+}typeStack;
+typeStack* stack_init(int RequestedDepth){
+   typeStackElement* ptrBuffer = (typeStackElement*)calloc(
+         RequestedDepth
+      ,  sizeof(typeStackElement)
+   );
+   typeStack* ptrStack = (typeStack*)calloc(
+         1
+      ,  sizeof(typeStack)
+   );
+   ptrStack->StackPointer = 0;
+   ptrStack->StackDepth   = RequestedDepth;
+   ptrStack->ptrBuffer    = ptrBuffer;
+   return ptrStack;
+}
+void stack_deinit(typeStack* ptrStack){
+   free(ptrStack->ptrBuffer);
+   free(ptrStack);
+}
+void stack_push(
+      typeStack*       ptrStack
+   ,  typeStackElement Element
+){
+   if(
+         ptrStack->StackPointer
+      <  ptrStack->StackDepth
+   ){
+      ptrStack->ptrBuffer[ptrStack->StackPointer++] = Element;
+   }
+}
+typeStackElement stack_pop(typeStack* ptrStack){
+   if(
+         0
+      <  ptrStack->StackPointer
+   ){
+      return ptrStack->ptrBuffer[--ptrStack->StackPointer];
+   }
+   return UndefinedElement;
+}
+int stack_size(typeStack* ptrStack){return ptrStack->StackPointer;}
+#if(1 == _DEBUG_)
+void stack_print(typeStack* ptrStack){
+   printf("\nCurrent stack:");
+   for(int i=0;i<ptrStack->StackPointer;i++){
+      printf(" %c", ptrStack->ptrBuffer[i]); // Hint: take care of typeStackElement
+   }
+}
+#endif
+
+#include <string.h>
+#include <stdbool.h>
+int solution(char *S){
+   int i,l=strlen(S);
+   if(l%2) return 0;
+   bool fail_safe=false;
+   typeStack* pS=stack_init(l);
+   for(i=0;i<l;i++){
+           if('('==S[i])stack_push(pS,S[i]);
+      else if('('!=stack_pop(pS)){fail_safe=true;break;}
+   }
+   int r=((true==fail_safe)||(0!=stack_size(pS)))?0:1;
+   stack_deinit(pS);
+   return r;
+}
---
+++ L7SQ03_Nesting/solution.h
@@ -0,0 +1,9 @@
+#ifndef _SOLUTION_H
+#define _SOLUTION_H
+
+#define _DEBUG_ 1
+
+extern int solution(char *S);
+
+#else
+#endif
