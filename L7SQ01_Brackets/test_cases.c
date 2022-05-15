d617cd391e5b80a46b20f47e14651eee421e0387
NAAGRAAJ HM <nagaraja.hm.0001@gmail.com>
2022-05-15 21:34:32 +0200

    Add files via upload

---
+++ L7SQ01_Brackets/test_cases.c
@@ -0,0 +1,6 @@
+#include "test_cases.h"
+
+char *S[] = {
+      "{[()()]}"
+   ,  "([)()]"
+};
---
+++ L7SQ01_Brackets/test_cases.h
@@ -0,0 +1,9 @@
+#ifndef _TEST_CASES_H
+#define _TEST_CASES_H
+
+#define NUMBER_OF_TEST_CASES 2
+
+extern char *S[];
+
+#else
+#endif
